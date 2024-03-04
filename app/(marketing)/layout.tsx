a2759675b4c638d788dd4c5a0157c272ea61d98c
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-03-04 21:15:13 +0100

    03: marketing skeleton

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,20 @@
+import { Footer } from "./footer";
+import { Header } from "./header";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MarketingLayout = ({ children }: Props) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button size="lg">Click me</Button>
+      This is a marketring page
     </div>
   )
 }
