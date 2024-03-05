2d9ce87005122f5c52db40b0c0059c36862b3dff
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-03-06 00:40:12 +0100

    05: footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, practice, and master new languages with Lingo.
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
