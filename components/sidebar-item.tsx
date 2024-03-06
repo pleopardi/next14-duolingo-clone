e8661f16207917a5456c65055f0ce9e2a9222094
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-03-06 16:09:29 +0100

    07: sidebar

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  label: string;
+  iconSrc: string;
+  href: string;
+};
+
+export const SidebarItem = ({
+  label,
+  iconSrc,
+  href,
+}: Props) => {
+  const pathname = usePathname();
+  const active = pathname === href;
+
+  return (
+    <Button
+      variant={active ? "sidebarOutline"  : "sidebar"}
+      className="justify-start h-[52px]"
+      asChild
+    >
+      <Link href={href}>
+        <Image
+          src={iconSrc}
+          alt={label}
+          className="mr-5"
+          height={32}
+          width={32}
+        />
+        {label}
+      </Link>
+    </Button>
+  );
+};
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+import {
+  ClerkLoading,
+  ClerkLoaded,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
 import { cn } from "@/lib/utils";
+
+import { SidebarItem } from "./sidebar-item";
 
 type Props = {
   className?: string;
@@ -7,10 +18,47 @@ type Props = {
 export const Sidebar = ({ className }: Props) => {
   return (
     <div className={cn(
-      "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+      "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
       className,
     )}>
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem 
+          label="Learn" 
+          href="/learn"
+          iconSrc="/learn.svg"
+        />
+        <SidebarItem 
+          label="Leaderboard" 
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem 
+          label="quests" 
+          href="/quests"
+          iconSrc="/quests.svg"
+        />
+        <SidebarItem 
+          label="shop" 
+          href="/shop"
+          iconSrc="/shop.svg"
+        />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
