304a2ddee0783248a612d77de99bdf064459ce44
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-03-11 17:33:54 +0100

    12: seed script

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-
-import { Button } from "@/components/ui/button";
 import { InfinityIcon } from "lucide-react";
 
+import { courses } from "@/db/schema";
+import { Button } from "@/components/ui/button";
+
 type Props = {
-  activeCourse: { imageSrc: string; title: string }; // TODO: Replace with DB types
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
---
+++ scripts/seed.ts
@@ -0,0 +1,49 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!); 
+// @ts-ignore
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Seeding database");
+
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: "Spanish",
+        imageSrc: "/es.svg",
+      },
+      {
+        id: 2,
+        title: "Italian",
+        imageSrc: "/it.svg",
+      },
+      {
+        id: 3,
+        title: "French",
+        imageSrc: "/fr.svg",
+      },
+      {
+        id: 4,
+        title: "Croatian",
+        imageSrc: "/hr.svg",
+      },
+    ]);
+
+    console.log("Seeding finished");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed the database");
+  }
+};
+
+main();
+
