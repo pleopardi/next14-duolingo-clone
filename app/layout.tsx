c81560b9c6974932f7652edda8abe9cbf62dc867
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-03-12 21:25:42 +0100

    18: exit modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from "@/components/ui/sonner";
+import { ExitModal } from "@/components/modals/exit-modal";
 import "./globals.css";
 
 const font = Nunito({ subsets: ["latin"] });
@@ -21,6 +22,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={font.className}>
           <Toaster />
+          <ExitModal />
           {children}
         </body>
       </html>
--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { InfinityIcon, X } from "lucide-react";
 
 import { Progress } from "@/components/ui/progress";
+import { useExitModal } from "@/store/use-exit-modal";
 
 type Props = {
   hearts: number;
@@ -14,10 +15,12 @@ export const Header = ({
   percentage,
   hasActiveSubscription,
 }: Props) => {
+  const { open } = useExitModal();
+
   return (
     <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
       <X
-        onClick={() => {}} // TODO: Add onClick exit
+        onClick={open}
         className="text-slate-500 hover:opacity-75 transition cursor-pointer"
       />
       <Progress value={percentage} />
