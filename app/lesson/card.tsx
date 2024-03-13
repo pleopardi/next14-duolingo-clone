b4eaf85601bd86c9abcf9bef98fa2f0f45df45bd
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-03-13 21:13:42 +0100

    20: challenge footer

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { useCallback } from "react";
+import { useAudio, useKey } from "react-use";
 
 import { cn } from "@/lib/utils";
 import { challenges } from "@/db/schema";
@@ -28,9 +30,20 @@ export const Card = ({
   disabled,
   type,
 }: Props) => {
+  const [audio, _, controls] = useAudio({ src: audioSrc || "" });
+
+  const handleClick = useCallback(() => {
+    if (disabled) return;
+
+    controls.play();
+    onClick();
+  }, [disabled, onClick, controls]);
+
+  useKey(shortcut, handleClick, {}, [handleClick]);
+
   return (
     <div
-      onClick={() => {}}
+      onClick={handleClick}
       className={cn(
         "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
         selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
@@ -42,6 +55,7 @@ export const Card = ({
         type === "ASSIST" && "lg:p-3 w-full"
       )}
     >
+      {audio}
       {imageSrc && (
         <div
           className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full"
--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { challengeOptions, challenges } from "@/db/schema";
 
 import { Header } from "./header";
+import { Footer } from "./footer";
 import { Challenge } from "./challenge";
 import { QuestionBubble } from "./question-bubble";
 
@@ -34,8 +35,17 @@ export const Quiz = ({
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
 
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
+
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+
+    setSelectedOption(id);
+  };
 
   const title = challenge.type === "ASSIST" 
     ? "Select the correct meaning"
@@ -60,9 +70,9 @@ export const Quiz = ({
               )}
               <Challenge
                 options={options}
-                onSelect={() => {}}
-                status="none"
-                selectedOption={undefined}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -70,6 +80,11 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer
+        disabled={!selectedOption}
+        status={status}
+        onCheck={() => {}}
+      />
     </>
   );
 };
